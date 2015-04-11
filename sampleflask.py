2d5eeb3b687285273fa69c655d88baa72d26928b
Chan Arbon <ceva1116@gmail.com>
2015-04-11 16:29:47 +0800

    Chan's Winning Commit 2

--- sampleflask.py
+++ sampleflask.py
@@ -22,7 +22,7 @@ def about():
 	""" % (flask.url_for("index"), flask.url_for("about"), flask.url_for("contact"))
 	return html
 	
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
 	html = """
 	<a href='%s'>Home</a><br>
@@ -32,7 +32,11 @@ def contact():
 	Email: cearbon@netsuite.com<br>
 	Message: Ping me if you need help with regards to NetSuite<br>
 	""" % (flask.url_for("index"), flask.url_for("about"), flask.url_for("contact"))
-	return html
+	name = flask.request.form.get('name')
+	message = flask.request.form.get('message')
+	email = flask.request.form.get('email')
+	return flask.render_template('hello.html', name=name, email=email, message=message)
+	#return html
 
 	
 if __name__=="__main__":
